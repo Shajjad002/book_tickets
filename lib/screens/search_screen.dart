ac8f2b6a329ea066001bdbc091590fe4ed74c695
Shariful islam <sishajjad@shakti.org.bd>
2023-12-20 20:00:08 +0600

    Search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:book_tickets/utils/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -20,22 +21,51 @@ class SearchScreen extends StatelessWidget {
             "What are \nyou looking for?",
             style:Styles.headLineStyle.copyWith(fontSize: 35),),
             const Gap(20),
+          FittedBox(
+           child: Container(
+              padding: const EdgeInsets.all(3.5),
+              child: Row(
+                children: [
+                  //Airline Tickets
+                  Container(
+                    width: size.width*.49,
+                    padding: EdgeInsets.symmetric(vertical: 7),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.horizontal(left: Radius.circular(50) ),
+                        color: Colors.white
+                    ),
+                    child: Center(child: Text("AirLine Tickets"),),
+                  ),
+                  //Hotels
+                  Container(
+                    width: size.width*.49,
+                    padding: EdgeInsets.symmetric(vertical: 7),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.horizontal(right: Radius.circular(50) ),
+                      color: Colors.transparent
+                    ),
+                    child: Center(child: Text("Hotels"),),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: const Color(0xFFF4F6FD)
+              ),
+            ),
+          ),
+          const Gap(20),
           Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color(0xFFF4F6FD),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
             child: Row(
               children: [
-                Container(
-                  width: size.width*.44,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.white
-                  ),
-                  child: Text("AirLine Tickets"),
-                ),
+                const Icon(Icons.flight_takeoff_rounded, color: Color(0xFFBFC205)),
+                Text("Departure",style: Styles.headLineStyle4 ,)
               ],
-            ),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: const Color(0xFFF4F6FD)
             ),
           ),
        ],
