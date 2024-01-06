42186658dc29870074c91da61960871e7ab66679
Shariful islam <sishajjad@shakti.org.bd>
2024-01-06 23:11:50 +0600

    Search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -76,14 +76,24 @@ class SearchScreen extends StatelessWidget {
         ),
           const Gap(35),
           const AppDoubleTextWidget(bigText: "Upcoming Flights", smallText: "View all"),
+          const Gap(15),
       Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
-            height:400 ,
+            height:410 ,
             width: size.width*0.42,
+            padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12),
               color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade200,
+                  blurRadius: 1,
+                  spreadRadius: 1,
+                ),
+              ],
             ),
             child: Column(
               children: [
@@ -91,16 +101,93 @@ class SearchScreen extends StatelessWidget {
                     height:190,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(12),
-                      image: DecorationImage(
+                      image: const DecorationImage(
                           fit: BoxFit.cover,
                           image: AssetImage(
                               "assets/images/sit.jpg"
                           )
                       )
-                  )
-                )
+                  ),
+                ),
+                Gap(12),
+                Text(
+                  "20% Discount on business class tickets from Airline On International.",
+                  style: Styles.headLineStyle3,
+                ),
               ],
             ),
+          ),
+          Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: size.width*0.44,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Color(0xFF3ABBB8),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Discount\nfor Survey",style: Styles.headLineStyle2.copyWith(fontWeight: FontWeight.bold, color: Colors.white,)),
+                        Gap(10),
+                        Text("Take the survey about out service & get discount",style: Styles.headLineStyle3.copyWith(fontWeight: FontWeight.w400, color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    right: -40,
+                    top: -35,
+                    child: Container(
+                    padding: EdgeInsets.all(30),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(width: 18,color: Color(0xFF189999)),
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  ),
+                ],
+              ),
+              Gap(12),
+              Container(
+                width: size.width*0.44,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Color(0xFFEC6545),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                child: Column(
+                  //crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Take Love",style: Styles.headLineStyle2.copyWith(fontWeight: FontWeight.bold, color: Colors.white,)),
+                    Gap(10),
+                    RichText(
+                        text: const TextSpan(
+                          children: [
+                            TextSpan(
+                              text: '😍',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            TextSpan(
+                              text: '🥰',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                            TextSpan(
+                              text: '😘',
+                              style: TextStyle(fontSize: 30),
+                            ),
+                          ]
+                        )
+                    ),
+                  ],
+                ),
+              ),
+            ],
           )
         ],
       )
